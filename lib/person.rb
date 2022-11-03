497750bf037e5d0173ee4a100129ffffebc851c9
Ashley <ashleynjeri7@gmail.com>
2022-11-03 12:58:09 +0300

    mass assignment

--- lib/person.rb
+++ lib/person.rb
@@ -1,5 +1,12 @@
 class Person
-  def initialize (hash_attributes)
+  attr_accessor :name, :birthday, :hair_color, :eye_color, :height,
+  :weight, :handed, :complexion, :t_shirt_size,
+  :wrist_size, :glove_size, :pant_length, :pant_width
+  
 
+  def initialize (hash_attributes)
+    hash_attributes.each do |key,value|
+      self.send("#{key}=",value)
+    end
   end
 end
